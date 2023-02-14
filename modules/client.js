43248aaa2762326360c3d1e19f7a1aa7f44a5644
TooFam0us <jere.syrjanen@gmail.com>
2023-02-14 10:24:12 +0200

    jeppisjepulis

--- modules/client.js
+++ modules/client.js
@@ -1,13 +1,18 @@
 var socket=io();
 
-var dl = document.getElementById('download');
-var download2 = document.getElementById('download2');
-dl.addEventListener('click',function() {httpGet("file","Filetype=a&Id=63ea5e18a2464ed1b87bdf6b")});
-download2.addEventListener('click',function() {httpGet("file","Filetype=b&Id=63ea7fa64cf78b991d1ad822")});
 
-socket.on("load", (data) => {
+const arrData = socket.on("load", (data) => {
 	console.log(data);
 });
+arrData.forEach(data => {
+	var item = document.createElement("<li>DownloadableFile1<button id='download'>Download</button></li>");
+	item.addEventListener('click', function() {httpGet("file","Id="+data._id)});
+	document.getElementById("listDownload").appendChild(item);
+});
+// download2.addEventListener('click',function() {httpGet("file","Filetype=b&Id=63ea7fa64cf78b991d1ad822")});
+// dl.addEventListener('click',function() {httpGet("file","Filetype=a&Id=63ea5e18a2464ed1b87bdf6b")});
+// var download2 = document.getElementById('download2');
+// var dl = document.getElementById('download');
 
 function deldata(){
 	socket.emit('delete','all');
